240e93f9865a22d1d5c05123eaa4201af36bd1cb
josemroca-ship-it <josem.roca@megafy.es>
2025-10-31 18:44:20 +0100

    Change GREETING message to Spanish

--- lib/config.ts
+++ lib/config.ts
@@ -15,7 +15,7 @@ export const STARTER_PROMPTS: StartScreenPrompt[] = [
 
 export const PLACEHOLDER_INPUT = "Ask anything...";
 
-export const GREETING = "How can I help you today?";
+export const GREETING = "En qué te puedo ayudar hoy?";
 
 export const getThemeConfig = (theme: ColorScheme): ThemeOption => ({
   color: {
